9bc69acf728c94dda8064be46a54d00aeae920cf
HolaMundoJava <eduperz38@gmail.com>
2023-04-19 10:42:52 +0100

    Sumatorio de 10 numeros

--- src/main/java/org/example/sumatorio.java
+++ src/main/java/org/example/sumatorio.java
@@ -1,11 +1,9 @@
 package org.example;
 
 public class sumatorio {
-    private int[] num;
-    private int resultant;
+    private final int resultant;
 
     public sumatorio(int[] nums) {
-        this.num = nums;
         int acc = 0;
         int i = 0;
         while (i < 10) {
